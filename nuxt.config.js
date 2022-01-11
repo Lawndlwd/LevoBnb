321a6cadaab7e9524f07128aa3a0269c06c0bff9
Levende <levende.mohammad@gravity.earth>
2022-01-11 19:30:18 +0100

    initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,83 @@
+export default {
+  components: true,
+  head: {
+    titleTemplate: 'LevoBnB: %s',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    bodyAttrs: {
+      class: ['my-style'],
+    },
+    meta: [
+      {
+        charset: 'utf-8',
+      },
+    ],
+  },
+  router: {
+    prefetchLinks: false,
+  },
+  plugins: [
+    '~/plugins/maps.client',
+    '~/plugins/dataApi',
+    '~/plugins/auth.client',
+    '~/plugins/vCalendar.client',
+    '~/plugins/stripe.client',
+  ],
+  modules: [
+    '~/modules/auth',
+    '~/modules/algolia',
+    '~/modules/cloudinary',
+    '@nuxtjs/cloudinary',
+    '~/modules/stripe',
+  ],
+  buildModules: ['@nuxtjs/tailwindcss', '@nuxt/image'],
+  cloudinary: {
+    cloudName: 'dy9jmzxjx',
+  },
+  image: {
+    cloudinary: {
+      baseURL: 'https://res.cloudinary.com/dy9jmzxjx/image/upload/',
+    },
+  },
+  css: ['~/assets/sass/app.scss'],
+  build: {
+    extractCSS: true,
+    loaders: {
+      limit: 0,
+    },
+  },
+  publicRuntimeConfig: {
+    rootUrl:
+      process.env.NODE_ENV === 'production'
+        ? 'https://nuxtbnb.com'
+        : 'http://localhost:3000',
+    auth: {
+      cookieName: 'idToken',
+      clientId:
+        '586769034827-u6fvionv6mh7sr95se524j95u8cl8htm.apps.googleusercontent.com',
+    },
+    algolia: {
+      appId: '3LWUG0HA7L',
+      key: '1cd64085e2f8f1d343257c7f760f44a8',
+    },
+    cloudinary: {
+      apiKey: '466848933876658',
+    },
+    stripe: {
+      key: 'pk_test_kJej6mAAN0hPu1ffCPQpinxq00X6Dbpvch',
+    },
+  },
+  privateRuntimeConfig: {
+    algolia: {
+      appId: '3LWUG0HA7L',
+      key: process.env.ALGOLIA_SECRET_KEY,
+    },
+    cloudinary: {
+      apiSecret: process.env.CLOUDINARY_SECRET_KEY,
+    },
+    stripe: {
+      secretKey: process.env.STRIPE_SECRET_KEY,
+    },
+  },
+}
---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="app">
+    <div class="app-container">
+      <div class="app-hero">
+        <h2>
+          - Settle in somewhere new. Discover stays to live, work, or
+          just relax.
+        </h2>
+      </div>
+    </div>
+    <SectionPremiumListings />
+    <SectionRecentlyAdded class="app-background-grey" :homes="homes" />
+    <SectionMore />
+  </div>
+</template>
+<script>
+export default {
+  head() {
+    return {
+      title: 'Homepage',
+      meta: [
+        {
+          name: 'description',
+          content:
+            'LevoBNB is an AirBNB app that uses Nuxt, Cloudinary, Stripe, Google Maps and Algolia.',
+          hid: 'description',
+        },
+      ],
+    }
+  },
+
+  async asyncData({ $dataApi }) {
+    return {
+      homes: (await $dataApi.getHomes()).json.hits,
+    }
+  },
+}
+</script>
